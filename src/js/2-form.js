fa273d582f9b44df98a18f19e2ebb5c080d5695b
NatalyMit <mitenkova91@icloud.com>
2024-01-03 22:42:22 +0100

    add corrects

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,25 +1,36 @@
 const formEl = document.querySelector('.feedback-form');
 formEl.addEventListener('input', onFormData);
 formEl.addEventListener('submit', onSubmitForm);
-const formData = {};
+
 function onFormData(e) {
-  formData[e.target.name] = e.target.value;
+  const formData = {
+    email: formEl.elements.email.value.trim(),
+    message: formEl.elements.message.value.trim(),
+  };
+
   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 }
 
 function onSubmitForm(e) {
   e.preventDefault();
-  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
-  e.currentTarget.reset();
-  localStorage.removeItem('feedback-form-state');
-}
-
-(function dataFromLocalStorage() {
-  const data = JSON.parse(localStorage.getItem('feedback-form-state'));
-  const email = document.querySelector('.feedback-form input');
-  const message = document.querySelector('.feedback-form textarea');
-  if (data) {
-    email.value = data.email;
-    message.value = data.message;
+  const emailValue = formEl.elements.email.value;
+  const messageValue = formEl.elements.message.value;
+  if (!emailValue || !messageValue) {
+    alert('Заповніть всі поля!');
+    return;
   }
-})();
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  localStorage.removeItem('feedback-form-state');
+  e.currentTarget.reset();
+}
+dataFromLocalStorage();
+function dataFromLocalStorage() {
+  const data = JSON.parse(localStorage.getItem('feedback-form-state'));
+  // const email = document.querySelector('.feedback-form input');
+  // const message = document.querySelector('.feedback-form textarea');
+  if (data !== null) {
+    const parsedSavedData = JSON.parse(data);
+    form.elements.email.value = parsedSavedData.email ?? '';
+    form.elements.message.value = parsedSavedData.message ?? '';
+  }
+}
